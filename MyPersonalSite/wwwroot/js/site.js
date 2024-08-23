ebf2e44bf3db2878cc10c3eec637dc97fbdeb08d
Pear <20259871+TheRealPear@users.noreply.github.com>
2024-08-23 13:41:44 -0400

    Basic dark mode

--- MyPersonalSite/wwwroot/js/site.js
+++ MyPersonalSite/wwwroot/js/site.js
@@ -2,38 +2,84 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const themeName = 'dark-theme';
-const itemDivs = document.querySelectorAll('.dark-theme');
-const themeSwitcher = document.getElementById('theme-switcher');
-const darkTheme = localStorage.getItem(themeName);
 
-if(darkTheme === null){
-    localStorage.setItem(themeName,"active");
-} else if(darkTheme === "active"){
-    addTheme();
-} else {
-    removeTheme();
-}
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2024 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
 
-themeSwitcher.addEventListener('click',function (){
-    let currentTheme = localStorage.getItem(themeName);
-    if(currentTheme === "inactive"){
-        localStorage.setItem(themeName,"active");
-        addTheme();
-    } else if (currentTheme === "active"){
-        localStorage.setItem(themeName,"inactive");
-        removeTheme();
+(() => {
+    'use strict'
+
+    const getStoredTheme = () => localStorage.getItem('theme')
+    const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+    const getPreferredTheme = () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme) {
+            return storedTheme
+        }
+
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
     }
-})
 
-function removeTheme(){
-    itemDivs.forEach(itemDiv => {
-        itemDiv.classList.remove(themeName);
-    });
-}
+    const setTheme = theme => {
+        if (theme === 'auto') {
+            document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme)
+        }
+    }
 
-function addTheme(){
-    itemDivs.forEach(itemDiv => {
-        itemDiv.classList.add(themeName);
-    });
-}
+    setTheme(getPreferredTheme())
+
+    const showActiveTheme = (theme, focus = false) => {
+        const themeSwitcher = document.querySelector('#bd-theme')
+
+        if (!themeSwitcher) {
+            return
+        }
+
+        // const themeSwitcherText = document.querySelector('#bd-theme-text')
+        const activeThemeIcon = document.querySelector('.theme-icon-active use')
+        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+        // const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+
+        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+            element.classList.remove('active')
+            element.setAttribute('aria-pressed', 'false')
+        })
+
+        btnToActive.classList.add('active')
+        btnToActive.setAttribute('aria-pressed', 'true')
+        // activeThemeIcon.setAttribute('href', svgOfActiveBtn)
+        // const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
+        // themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
+
+        if (focus) {
+            themeSwitcher.focus()
+        }
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme !== 'light' && storedTheme !== 'dark') {
+            setTheme(getPreferredTheme())
+        }
+    })
+
+    window.addEventListener('DOMContentLoaded', () => {
+        showActiveTheme(getPreferredTheme())
+
+        document.querySelectorAll('[data-bs-theme-value]')
+            .forEach(toggle => {
+                toggle.addEventListener('click', () => {
+                    const theme = toggle.getAttribute('data-bs-theme-value')
+                    setStoredTheme(theme)
+                    setTheme(theme)
+                    showActiveTheme(theme, true)
+                })
+            })
+    })
+})()
